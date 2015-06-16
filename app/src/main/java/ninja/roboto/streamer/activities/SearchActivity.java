04de8d9bc767409101aab1988497b2202547c5a1
Douglas Drumond <douglas@cafelinear.com>
2015-06-16 09:57:20 -0300

    Add SearchActivity
    Signed-off-by: Douglas Drumond <douglas@cafelinear.com>

---
+++ app/src/main/java/ninja/roboto/streamer/activities/SearchActivity.java
@@ -0,0 +1,16 @@
+package ninja.roboto.streamer.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ninja.roboto.streamer.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+}
