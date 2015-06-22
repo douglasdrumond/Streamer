fe8bacb5962b2354fb6c56fd0cc103d8aadce33f
Douglas Drumond <douglas@cafelinear.com>
2015-06-21 22:42:50 -0300

    remove unused menu
    Signed-off-by: Douglas Drumond <douglas@cafelinear.com>

--- app/src/main/java/ninja/roboto/streamer/activities/TopTracksActivity.java
+++ app/src/main/java/ninja/roboto/streamer/activities/TopTracksActivity.java
@@ -1,9 +1,7 @@
 package ninja.roboto.streamer.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 
 import ninja.roboto.streamer.R;
 
@@ -13,27 +11,5 @@ public class TopTracksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_tracks);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_top_tracks, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
